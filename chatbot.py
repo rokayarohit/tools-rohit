966967564a69d19acf68a2fa628c30721aa5df28
Rohit Rokaya <109261992+rokayarohit@users.noreply.github.com>
2022-07-29 23:19:17 +0545

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+#this tool is for fun purpose only
+import colorama
+from colorama import Fore
+
+print(Fore.RED + 'Born To Learn')
+a=input("What is your name? :")
+b=int(input("Hello",a,"How old are you?:"))
+      if b>16:
+      print("You are eligible to use this tool.")
+      c=input("Are you ready to chat with me?(Y/N):")
+      if c=="Y":
+      d=input("Are you male or female?:")
+      if d=="male":
+      print("Go and read your otherwise don't waste your time")
+      else:
+        print("Go and learn something new don't waste your important time")
+      else:
+        print("You are very good person")
+      else:
+        print("Jau padhai likhai me dhyan do doctor bano samjhe tum")
